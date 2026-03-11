5b3737cb1d6fcd67b493899ab96637aed3314c33
Josiah Parry <josiah.parry@gmail.com>
2026-03-11 06:51:51 -0700

    update

--- plenary/trace-downstream.R
+++ plenary/trace-downstream.R
@@ -2,14 +2,14 @@ trace_downstream <- function(input, token = arc_token()) {
   arc_gp_job$new(
     base_url = "https://hydro.arcgis.com/arcgis/rest/services/Tools/Hydrology/GPServer/TraceDownstream",
     params = list(
-      InputPoints = arcgisutils::as_esri_featureset(sf::st_cast(
+      InputPoints = as_esri_featureset(st_cast(
         input,
         "POINT"
       )),
       Generalize = "true",
       f = "json"
     ),
-    result_fn = arcgisutils::parse_gp_feature_record_set,
+    result_fn = parse_gp_feature_record_set,
     token = token
   )
 }
